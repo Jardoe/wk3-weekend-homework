21446105e9f2e2520dd620c237b866ab2098262d
Jardoe <jussi.g.pardoe@gmail.com>
2018-02-16 12:11:13 +0000

    added delete_all to all classes

---
+++ models/customers.rb
@@ -0,0 +1,34 @@
+require_relative("../db/sql_runner.rb")
+
+class Customer
+  attr_reader :funds
+  attr_accessor :id, :name
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @funds = options['funds'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO customers
+      (
+        name,
+        funds
+      )
+      VALUES
+      (
+        $1,$2
+      )
+      RETURNING id"
+      values = [@name, @funds]
+      customer = SqlRunner.run(sql, values).first()
+      @id = customer['id'].to_i
+  end
+
+  def Customer.delete_all()
+    sql = "DELETE FROM customers"
+    SqlRunner.run(sql)
+
+  end
+end
---
+++ models/tickets.rb
@@ -0,0 +1,37 @@
+require_relative('../db/sql_runner.rb')
+
+class Ticket
+
+  attr_reader :id
+  attr_accessor :customer_id, :film_id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @customer_id = options['customer_id'].to_i
+    @film_id = options['film_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO tickets
+    (customer_id, film_id)
+    VALUES ($1, $2)
+    RETURNING id;"
+    values = [@customer_id, @film_id]
+    ticket = SqlRunner.run(sql, values).first()
+    @id = ticket['id'].to_i
+  end
+
+
+  def Ticket.all()
+    sql = "SELECT * FROM tickets"
+    values = []
+    tickets = SqlRunner.run(sql, values)
+    return tickets.map { |ticket| Ticket.new(ticket)  }
+
+  end
+
+  def Ticket.delete_all()
+    sql = "DELETE FROM tickets"
+    SqlRunner.run(sql)
+  end
+end
